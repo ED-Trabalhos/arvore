c3af5addbe1f86b515115ad11b7ed39ae676670c
Andreas Gunther Matos Leal <andreasgleal@gmail.com>
2025-11-16 21:17:06 -0300

    feat: NIL node

--- red-black/arvore.h
+++ red-black/arvore.h
@@ -2,3 +2,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef enum { RED, BLACK } Color;
+
+typedef struct Node {
+    int key;
+    Color color;
+    struct Node *left;
+    struct Node *right;
+    struct Node *parent;
+} Node;
+
+// This tells the compiler, "This variable exists, but it's defined in another file. Just trust me."
+extern Node* NIL;
+
+void initialize_NIL();
+Node* insert(Node *root, int key);
+void inorder_transversal(Node *root);
